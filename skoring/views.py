8f715e329739966e411ba77f8f73fd4defd24ca7
Luthfi A. Ardhani <luthfi@insight.co>
2022-02-10 21:13:11 +0700

    build index view

--- skoring/views.py
+++ skoring/views.py
@@ -1,3 +1,20 @@
+from importlib.metadata import requires
+from django.http import HttpResponse
 from django.shortcuts import render
+# from .learning import analyze
 
-# Create your views here.
+def index(request):
+    return render(request, 'skoring/index.html')
+
+def analyze(request):
+    query = request.POST['query']
+    topic = request.POST['topic']
+    if query and topic:
+        # analyzing = analyze.train(query, topic)
+        data = {
+            'result': 810,
+            'query': query,
+            'topic': topic
+        }
+        print(data)
+        return render(request, 'skoring/index.html', data)
